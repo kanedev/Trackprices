3ab920ffb99c24c8866ff6ea31f0427a1c1f488f
kanedev <kanedev@live.fr>
2020-02-02 10:49:30 +0100

    🚨: 🚧 work in progress 🚧

--- src/js/modules/test.js
+++ src/js/modules/test.js
@@ -1,2 +1,20 @@
+function test(){
+    return new Promise((resolve, reject) => {
+           setTimeout(() => {
+ resolve(2000)
+}, 5000);
+       })
+   
+   
+}
 
-
+async function asyncCall() {
+   var a=100;
+   console.log('a = ',a)
+   var b = await test();
+   var c=300;
+ 
+   console.log('b = ',b)
+   console.log('c = ',c)
+ }
+ asyncCall()
